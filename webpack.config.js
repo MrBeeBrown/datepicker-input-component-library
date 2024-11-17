209d1cd24977f0ebb2ae3f9f0fb5b7d7d924269c
MrBeeBrown <bruno.grosdesirs@outlook.fr>
2024-11-17 18:57:01 +0100

    création du composant datepicker-input

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'DatePickerInputComponantLibrary',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+};
